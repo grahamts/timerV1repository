4a01cfad6d3124ec16fdd38a3f2e4d7cb036bab5
Taylor Graham <grahamts@miamioh.edu>
2016-01-23 19:00:55 -0500

    update server

--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ module.exports.initialize = function(app) {
 //    app.get('/about', home.about);
       app.get('/loginform', home.loginform);
  //   app.get('/images/:image_id', image.index);
-    app.get('/timer', home.timer);
+//    app.get('/timer', home.timer);
     app.post('/images', image.create);
     app.post('/images/:image_id/like', image.like);
     app.post('/images/:image_id/comment', image.comment);
